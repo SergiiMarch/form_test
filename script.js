d254cf07963617ea3792f8d477848a7984677234
Sergii Marchenko <marikmarchelo@gmail.com>
2023-07-18 11:07:13 +0300

    add event form

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 const formEl = document.querySelector(".obratnuj-zvonok");
-console.dir(formEl);
+
+const KEY_STORAGE = "key_form";
+const selectedForm = {};
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(formEl);
+  formData.forEach((value, name) => console.log(value, name));
+});
+
+formEl.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  console.log(e.target.name);
+  selectedForm[e.target.name] = e.target.value;
+  console.log(selectedForm);
+  localStorage.setItem(KEY_STORAGE, JSON.stringify(selectedForm));
+});
