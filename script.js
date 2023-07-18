c229c378ef521052c96e9fb8be5a8e204c2d0c2a
Sergii Marchenko <marikmarchelo@gmail.com>
2023-07-18 21:36:10 +0300

    new commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const formEl = document.querySelector(".obratnuj-zvonok");
 
 const KEY_STORAGE = "selectedForm";
-const selectedForm = {};
+
 initForm();
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -10,16 +10,24 @@ formEl.addEventListener("submit", (e) => {
 });
 
 formEl.addEventListener("change", (e) => {
-  console.log(e.target.value);
-  console.log(e.target.name);
-  selectedForm[e.target.name] = e.target.value;
-  localStorage.setItem(KEY_STORAGE, JSON.stringify(selectedForm));
+  //   console.log(e.target.value);
+  //   console.log(e.target.name);
+  let persistetFilters = localStorage.getItem(KEY_STORAGE);
+  persistetFilters = persistetFilters ? JSON.parse(persistetFilters) : {};
+  persistetFilters[e.target.name] = e.target.value;
+
+  localStorage.setItem(KEY_STORAGE, JSON.stringify(persistetFilters));
 });
 
 function initForm() {
   let persistesFilters = localStorage.getItem(KEY_STORAGE);
   if (persistesFilters) {
     persistesFilters = JSON.parse(persistesFilters);
+    Object.entries(persistesFilters).forEach(([name, value]) => {
+      formEl.elements[name].value = value;
+      //   selectedForm = {};
+      //   localStorage.removeItem(KEY_STORAGE);
+    });
   }
-  console.log(persistesFilters);
 }
+console.log(KEY_STORAGE);
